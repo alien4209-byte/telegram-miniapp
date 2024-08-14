82569383345ecbb6cbad97ec455355254223e54c
Dimitri <accept@protonmail.ch>
2024-08-14 20:00:38 +0200

    D1 database better error loging

--- worker/db.ts
+++ worker/db.ts
@@ -3,12 +3,17 @@ import { TelegramUser } from './types/types';
 import * as dbTypes from './types/dbTypes';
 
 export async function getSetting(db: D1Database, settingName: string): Promise<string | null> {
-	const result = await db
-		.prepare('SELECT value FROM settings WHERE name = ?')
-		.bind(settingName)
-		.first<dbTypes.Setting>();
+	try {
+		const result = await db
+			.prepare('SELECT value FROM settings WHERE name = ?')
+			.bind(settingName)
+			.first<dbTypes.Setting>();
 
-	return result ? result.value : null;
+		return result ? result.value : null;
+	} catch (e: any) {
+		console.error({ message: e.message });
+		throw e;
+	}
 }
 
 export async function setSetting(
@@ -16,27 +21,37 @@ export async function setSetting(
 	settingName: string,
 	settingValue: string
 ): Promise<D1Result> {
-	return await db
-		.prepare(
-			`
-      INSERT INTO settings (created_date, updated_date, name, value)
-      VALUES (DATETIME('now'), DATETIME('now'), ?, ?)
-      ON CONFLICT(name) DO UPDATE SET
-        updated_date = DATETIME('now'),
-        value = excluded.value
-      WHERE excluded.value <> settings.value
-    `
-		)
-		.bind(settingName, settingValue)
-		.run();
+	try {
+		return await db
+			.prepare(
+				`
+          INSERT INTO settings (created_date, updated_date, name, value)
+          VALUES (DATETIME('now'), DATETIME('now'), ?, ?)
+          ON CONFLICT(name) DO UPDATE SET
+            updated_date = DATETIME('now'),
+            value = excluded.value
+          WHERE excluded.value <> settings.value
+        `
+			)
+			.bind(settingName, settingValue)
+			.run();
+	} catch (e: any) {
+		console.error({ message: e.message });
+		throw e;
+	}
 }
 
 export async function getLatestUpdateId(db: D1Database): Promise<number> {
-	const result = await db
-		.prepare('SELECT update_id FROM messages ORDER BY update_id DESC LIMIT 1')
-		.first<dbTypes.Message>();
+	try {
+		const result = await db
+			.prepare('SELECT update_id FROM messages ORDER BY update_id DESC LIMIT 1')
+			.first<dbTypes.Message>();
 
-	return result ? Number(result.update_id) : 0;
+		return result ? Number(result.update_id) : 0;
+	} catch (e: any) {
+		console.error({ message: e.message });
+		throw e;
+	}
 }
 
 export async function addMessage(
@@ -44,40 +59,55 @@ export async function addMessage(
 	message: string,
 	updateId: number
 ): Promise<D1Result> {
-	return await db
-		.prepare(
-			`
-      INSERT INTO messages (created_date, updated_date, message, update_id)
-      VALUES (DATETIME('now'), DATETIME('now'), ?, ?)
-    `
-		)
-		.bind(message, updateId.toString())
-		.run();
+	try {
+		return await db
+			.prepare(
+				`
+          INSERT INTO messages (created_date, updated_date, message, update_id)
+          VALUES (DATETIME('now'), DATETIME('now'), ?, ?)
+        `
+			)
+			.bind(message, updateId.toString())
+			.run();
+	} catch (e: any) {
+		console.error({ message: e.message });
+		throw e;
+	}
 }
 
 export async function getUser(db: D1Database, telegramId: number): Promise<dbTypes.User | null> {
-	return await db
-		.prepare('SELECT * FROM users WHERE telegram_id = ?')
-		.bind(telegramId)
-		.first<dbTypes.User>();
+	try {
+		return await db
+			.prepare('SELECT * FROM users WHERE telegram_id = ?')
+			.bind(telegramId)
+			.first<dbTypes.User>();
+	} catch (e: any) {
+		console.error({ message: e.message });
+		throw e;
+	}
 }
 
 export async function getUserByTokenHash(
 	db: D1Database,
 	tokenHash: Uint8Array
 ): Promise<dbTypes.User | null> {
-	return await db
-		.prepare(
-			`
-      SELECT u.*
-      FROM users u
-      JOIN tokens t ON u.id = t.user_id
-      WHERE t.token_hash = ? AND t.expired_date > DATETIME('now')
-      LIMIT 1
-    `
-		)
-		.bind(tokenHash)
-		.first<dbTypes.User>();
+	try {
+		return await db
+			.prepare(
+				`
+          SELECT u.*
+          FROM users u
+          JOIN tokens t ON u.id = t.user_id
+          WHERE t.token_hash = ? AND t.expired_date > DATETIME('now')
+          LIMIT 1
+        `
+			)
+			.bind(tokenHash)
+			.first<dbTypes.User>();
+	} catch (e: any) {
+		console.error({ message: e.message });
+		throw e;
+	}
 }
 
 export async function saveCalendar(
@@ -86,27 +116,37 @@ export async function saveCalendar(
 	calendarRef: string,
 	userId: number
 ): Promise<D1Result> {
-	return await db
-		.prepare(
-			`
-      INSERT INTO calendars (created_date, updated_date, calendar_json, calendar_ref, user_id)
-      VALUES (DATETIME('now'), DATETIME('now'), ?, ?, ?)
-    `
-		)
-		.bind(calendarJson, calendarRef, userId)
-		.run();
+	try {
+		return await db
+			.prepare(
+				`
+          INSERT INTO calendars (created_date, updated_date, calendar_json, calendar_ref, user_id)
+          VALUES (DATETIME('now'), DATETIME('now'), ?, ?, ?)
+        `
+			)
+			.bind(calendarJson, calendarRef, userId)
+			.run();
+	} catch (e: any) {
+		console.error({ message: e.message });
+		throw e;
+	}
 }
 
 export async function getCalendarByRef(
 	db: D1Database,
 	calendarRef: string
 ): Promise<string | null> {
-	const result = await db
-		.prepare('SELECT calendar_json FROM calendars WHERE calendar_ref = ?')
-		.bind(calendarRef)
-		.first<dbTypes.Calendar>();
+	try {
+		const result = await db
+			.prepare('SELECT calendar_json FROM calendars WHERE calendar_ref = ?')
+			.bind(calendarRef)
+			.first<dbTypes.Calendar>();
 
-	return result ? result.calendar_json : null;
+		return result ? result.calendar_json : null;
+	} catch (e: any) {
+		console.error({ message: e.message });
+		throw e;
+	}
 }
 
 export async function saveUserAndToken(
@@ -115,72 +155,72 @@ export async function saveUserAndToken(
 	auth_timestamp: number,
 	tokenHash: Uint8Array
 ): Promise<D1Result[]> {
-	const userStmt = db
-		.prepare(
-			`
-    INSERT INTO users (
-      created_date, updated_date, last_auth_timestamp, telegram_id,
-      is_bot, first_name, last_name, username, language_code,
-      is_premium, added_to_attachment_menu, allows_write_to_pm, photo_url
-    ) VALUES (
-      DATETIME('now'), DATETIME('now'), ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?
-    )
-    ON CONFLICT(telegram_id) DO UPDATE SET
-      updated_date = DATETIME('now'),
-      last_auth_timestamp = ?,
-      is_bot = ?,
-      first_name = ?,
-      last_name = ?,
-      username = ?,
-      language_code = ?,
-      is_premium = ?,
-      added_to_attachment_menu = ?,
-      allows_write_to_pm = ?,
-      photo_url = ?
-    WHERE ? > users.last_auth_timestamp
-  `
-		)
-		.bind(
-			auth_timestamp,
-			user.id, // This is telegram_id in the database
-			Number(user.is_bot),
-			user.first_name,
-			user.last_name || null,
-			user.username || null,
-			user.language_code || null,
-			Number(user.is_premium),
-			Number(user.added_to_attachment_menu),
-			Number(user.allows_write_to_pm),
-			user.photo_url || null,
-			// Repeat values for UPDATE
-			auth_timestamp,
-			Number(user.is_bot),
-			user.first_name,
-			user.last_name || null,
-			user.username || null,
-			user.language_code || null,
-			Number(user.is_premium),
-			Number(user.added_to_attachment_menu),
-			Number(user.allows_write_to_pm),
-			user.photo_url || null,
-			auth_timestamp // For the WHERE clause
-		);
-
-	const tokenStmt = db
-		.prepare(
-			`
-    INSERT INTO tokens (created_date, updated_date, expired_date, user_id, token_hash)
-    VALUES (DATETIME('now'), DATETIME('now'), DATETIME('now', '+1 day'),
-      (SELECT id FROM users WHERE telegram_id = ?), ?
-    )
-  `
-		)
-		.bind(user.id, tokenHash);
-
 	try {
+		const userStmt = db
+			.prepare(
+				`
+        INSERT INTO users (
+          created_date, updated_date, last_auth_timestamp, telegram_id,
+          is_bot, first_name, last_name, username, language_code,
+          is_premium, added_to_attachment_menu, allows_write_to_pm, photo_url
+        ) VALUES (
+          DATETIME('now'), DATETIME('now'), ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?
+        )
+        ON CONFLICT(telegram_id) DO UPDATE SET
+          updated_date = DATETIME('now'),
+          last_auth_timestamp = ?,
+          is_bot = ?,
+          first_name = ?,
+          last_name = ?,
+          username = ?,
+          language_code = ?,
+          is_premium = ?,
+          added_to_attachment_menu = ?,
+          allows_write_to_pm = ?,
+          photo_url = ?
+        WHERE ? > users.last_auth_timestamp
+      `
+			)
+			.bind(
+				auth_timestamp,
+				user.id,
+				Number(user.is_bot),
+				user.first_name,
+				user.last_name || null,
+				user.username || null,
+				user.language_code || null,
+				Number(user.is_premium),
+				Number(user.added_to_attachment_menu),
+				Number(user.allows_write_to_pm),
+				user.photo_url || null,
+				// Repeat values for UPDATE
+				auth_timestamp,
+				Number(user.is_bot),
+				user.first_name,
+				user.last_name || null,
+				user.username || null,
+				user.language_code || null,
+				Number(user.is_premium),
+				Number(user.added_to_attachment_menu),
+				Number(user.allows_write_to_pm),
+				user.photo_url || null,
+				auth_timestamp // For the WHERE clause
+			);
+
+		const tokenStmt = db
+			.prepare(
+				`
+        INSERT INTO tokens (created_date, updated_date, expired_date, user_id, token_hash)
+        VALUES (DATETIME('now'), DATETIME('now'), DATETIME('now', '+1 day'),
+          (SELECT id FROM users WHERE telegram_id = ?), ?
+        )
+      `
+			)
+			.bind(user.id, tokenHash);
+
 		return await db.batch([userStmt, tokenStmt]);
-	} catch (error) {
-		console.error('Error in saveUserAndToken:', error);
-		throw error;
+	} catch (e: any) {
+		console.error({ message: e.message });
+		throw e;
 	}
 }
