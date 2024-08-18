3956ee9dc537fba9b110a5163650c7a328ce14dc
Dimitri <accept@protonmail.ch>
2024-08-19 00:02:34 +0200

    fix calendar page

--- webapp/src/pages/InitializerPage/InitializerPage.tsx
+++ webapp/src/pages/InitializerPage/InitializerPage.tsx
@@ -1,4 +1,4 @@
-import React, { useMemo } from 'react';
+import React, { useMemo, useEffect } from 'react';
 import { useLaunchParams, useCloudStorage } from '@telegram-apps/sdk-react';
 import { useQuery, useMutation } from '@tanstack/react-query';
 import { Text, Button } from '@telegram-apps/telegram-ui';
@@ -115,20 +115,36 @@ const InitializerPage: React.FC = () => {
 		return 'en';
 	}, [data]);
 
+	useEffect(() => {
+		console.log('InitMiniApp Data:', data);
+		console.log('start_page:', data?.start_page);
+		console.log('start_param:', data?.start_param);
+		console.log('isOnboardingComplete:', isOnboardingComplete);
+	}, [data, isOnboardingComplete]);
+
+	if (isInitLoading || isStatusLoading) {
+		return <LoadingSpinner />;
+	}
+
+	if (errorMessage) {
+		return <ErrorMessage message={errorMessage} onRetry={refetch} />;
+	}
+
+	if (!data || !data.token) {
+		return <ErrorMessage message={ERROR_MESSAGES.TOKEN_MISSING} onRetry={refetch} />;
+	}
+
 	return (
-  <LanguageProvider languageCode={languageCode}>
-    {isInitLoading || isStatusLoading ? (
-      <LoadingSpinner />
-    ) : errorMessage ? (
-      <ErrorMessage message={errorMessage} onRetry={refetch} />
-    ) : data?.start_page === 'calendar' && data?.start_param ? (
-      <Calendar token={data!.token} apiRef={data.start_param} />
-    ) : isOnboardingComplete ? (
-      <Home token={data!.token} />
-    ) : (
-      <Onboarding onComplete={() => setOnboardingComplete(true)} />
-    )}
-  </LanguageProvider>
-);
+		<LanguageProvider languageCode={languageCode}>
+			{data.start_page === 'calendar' && data.start_param ? (
+				<Calendar token={data.token} apiRef={data.start_param} />
+			) : isOnboardingComplete ? (
+				<Home token={data.token} />
+			) : (
+				<Onboarding onComplete={() => setOnboardingComplete(true)} />
+			)}
+		</LanguageProvider>
+	);
+};
 
 export default InitializerPage;
