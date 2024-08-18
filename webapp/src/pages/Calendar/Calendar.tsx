03da5f443901bcf94b0ab3cb0bfb307c9eff65d1
Dimitri <accept@protonmail.ch>
2024-08-19 00:20:59 +0200

    fix calendar page

--- webapp/src/pages/Calendar/Calendar.tsx
+++ webapp/src/pages/Calendar/Calendar.tsx
@@ -1,40 +1,28 @@
-import React, { useEffect } from 'react';
+import React, { useState, useMemo } from 'react';
 import { DayPicker, SelectMultipleEventHandler } from 'react-day-picker';
 import { useQuery } from '@tanstack/react-query';
 import { Spinner, Text } from '@telegram-apps/telegram-ui';
+
 import { getCalendarByRef } from '@/api';
-import { CalendarProps, CalendarType } from '@/types/types';
+import { CalendarType, CalendarProps } from '@/types/types';
+
 import 'react-day-picker/dist/style.css';
 
 const Calendar: React.FC<CalendarProps> = ({ token, apiRef }) => {
-	const [selectedDates, setSelectedDates] = React.useState<Date[]>([]);
-	const [availableDates, setAvailableDates] = React.useState<Date[]>([]);
+	const [selectedDates, setSelectedDates] = useState<Date[]>([]);
 
 	const { data, isLoading, error } = useQuery<{ calendar: CalendarType }, Error>({
 		queryKey: ['calendar', apiRef],
 		queryFn: () => getCalendarByRef(token, apiRef),
 	});
 
-	useEffect(() => {
-		if (data && data.calendar && Array.isArray(data.calendar.dates)) {
-			console.log('API Response:', data);
-			const parsedDates = data.calendar.dates.map(dateStr => new Date(dateStr));
-			setAvailableDates(parsedDates);
-			console.log('Available Dates:', parsedDates);
-		}
+	const disabledMatcher = useMemo(() => {
+		if (!data) return () => false;
+		const enabledDates = new Set(data.calendar.dates);
+		return (date: Date) => !enabledDates.has(date.toISOString().split('T')[0]);
 	}, [data]);
 
-	const disabledDays = React.useCallback(
-		(date: Date) => {
-			return !availableDates.some(
-				availableDate =>
-					availableDate.toISOString().split('T')[0] === date.toISOString().split('T')[0]
-			);
-		},
-		[availableDates]
-	);
-
-	const handleDaySelect: SelectMultipleEventHandler = dates => {
+	const handleSelectDates: SelectMultipleEventHandler = dates => {
 		setSelectedDates(dates || []);
 	};
 
@@ -46,20 +34,12 @@ const Calendar: React.FC<CalendarProps> = ({ token, apiRef }) => {
 			<h2>Pick out of proposed dates</h2>
 			<DayPicker
 				mode="multiple"
+				weekStartsOn={1}
+				min={0}
 				selected={selectedDates}
-				onSelect={handleDaySelect}
-				disabled={disabledDays}
-				footer={
-					selectedDates.length > 0
-						? `You picked ${selectedDates.length} date(s): ${selectedDates.map(date => date.toDateString()).join(', ')}`
-						: 'Please pick one or more dates'
-				}
+				onSelect={handleSelectDates}
+				disabled={isLoading || disabledMatcher}
 			/>
-			<div>
-				<h3>Debug Information:</h3>
-				<p>Available Dates: {availableDates.map(d => d.toDateString()).join(', ')}</p>
-				<p>Selected Dates: {selectedDates.map(d => d.toDateString()).join(', ')}</p>
-			</div>
 		</div>
 	);
 };
